0a27cc2a06f30a3b3901f86eed48d023e4a4adc6
Ashe Austaire <AsheAustaire@gmail.com>
2017-08-25 14:22:35 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
+
     programmer_hash[:grace_hopper]
 
 end
@@ -36,8 +37,10 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+    }
+
     programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -56,7 +59,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages].join
+
+    programmer_hash[:dennis_ritchie][:languages].join()
+
 end
 
 def adding_matz
@@ -80,13 +85,13 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
-     programmer_hash[:yukihiro_matsumoto] = {
+    }
+    programmer_hash[:yukihiro_matsumoto] = {
       		:known_for => "Ruby",
       		:languages => ["LISP", "C"]
       	}
 
-      	programmer_hash
+    programmer_hash
 
 end
 
@@ -106,8 +111,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+
      programmer_hash[:alan_kay][:known_for] = "GUI"
 	 programmer_hash
 
@@ -128,7 +132,9 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+    }
+
     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
     programmer_hash
+
 end
