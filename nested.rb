c1c83197ceaa6890d37d3476ad7d0ea74502588d
david landau <thelandaus@gmail.com>
2018-05-10 03:09:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -89,12 +89,15 @@ def adding_matz
           :languages => ["C"]
         }
      }
+
      
       programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
  
- 
+    programmer_hash
     
 end
+     
+
 
 def changing_alan
 	programmer_hash = 
@@ -114,7 +117,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    
+    programmer_hash
      
 end
 
@@ -134,5 +140,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash[:dennis_ritchie][:languages]<< "Assembly"
+  programmer_hash
 end
