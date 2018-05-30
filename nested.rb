460e2a5a5708bde6820ea7c3258675ce1968d09f
Stephen F <stephen@farver.io>
2018-05-30 04:43:17 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
      
-  programmer_hash[:dennis_ritchie][:languages].to_s
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -83,11 +83,12 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = 
-    {
-      known_for: "Ruby",
-      languages: ["LISP, C"]
-    }
+  programmer_hash.merge!(
+    yukihiro_matsumoto: 
+      {
+        known_for: "Ruby",
+        languages: ["LISP, C"]
+      })
 end
 
 def changing_alan
@@ -109,7 +110,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-  programmer_hash[:alan_kay][:known_for] = ["GUI"]
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -129,5 +132,7 @@ def adding_to_dennis
         }
      }
   
-  programmer_hash[:dennis_ritchie][:known_for] = ["Assembly"]
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  
+  programmer_hash
 end
