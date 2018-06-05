f1958259351da63a2123c5f3a5bfd26c23259d29
Gabriel.Batista <gabedbatista@gmail.com>
2018-06-04 22:27:49 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,6 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:grace_hopper]
+
+
 
 
 end
@@ -37,6 +41,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +61,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -80,7 +88,9 @@ def adding_matz
         }
      }
 
-    
+    programmer_hash[:yukihiro_matsumoto] = {}
+
+    programmer_hash
 end
 
 def changing_alan
@@ -102,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for]= "#{alans_new_info}"
+     programmer_hash
      
 end
 
@@ -122,4 +134,7 @@ def adding_to_dennis
         }
      }
 
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+     programmer_hash
 end
