25cf5d77e2aa8c90fbd6034d235ef6948991f1c4
Bryan Teng <bteng1226@gmail.com>
2018-06-25 01:07:15 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -111,7 +111,7 @@ def changing_alan
      alans_new_info = "GUI"
      
     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -131,4 +131,5 @@ def adding_to_dennis
         }
      }
 programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+return programmer_hash
 end
