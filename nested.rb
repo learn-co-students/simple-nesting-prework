c09d4e56e7e25b7eea4fba5cdc31c6b2953d8e59
Dina Hafez <dinahafez.8@gmail.com>
2018-07-09 09:55:52 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -82,7 +82,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-    
+     
+  programmer_hash[:yukihiro_matsumoto] = {}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+  
+  programmer_hash
+  
 end
 
 def changing_alan
@@ -104,6 +110,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
@@ -123,5 +131,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 
 end
