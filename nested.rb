5e45b1a175f5f21e6f25944daddaf04f0c537944
Kelly <devkells00@gmail.com>
2018-07-13 23:33:04 -0500

    revised

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => ["Ruby"], :languages => ["LISP", "C"]}
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 	programmer_hash
 
 end
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info  #----- why reference the variable instead of the value? because it could be >1 value, or an array? this is the safer route?
 		 programmer_hash
 
 end
@@ -125,6 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly" #------ why << when all instructions say = "Assembly"? << pushes new value onto array list, and = would replace/override a value(s)?
 	programmer_hash
 end
