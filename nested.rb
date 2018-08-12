31faf715d8d3e39667e47c93d8120c97f26eae16
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-12 01:20:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,8 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP, C"]}
 
     
+    programmer_hash
 end
 
 def changing_alan
@@ -102,8 +104,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
@@ -123,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
