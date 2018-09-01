4e0d83b6f4e6209e0267cf6263b911b6975dbdc1
Aisha D'Souza <aishad.souza@yahoo.com>
2018-09-01 16:00:21 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,6 +83,11 @@ def adding_matz
         }
      }
 
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+  }
+  programmer_hash
     
 end
 
@@ -99,10 +107,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay][:known_for] = "GUI"
+
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+alans_new_info = "GUI"
+programmer_hash
      
-     
+
 end
 
 def adding_to_dennis
@@ -121,5 +133,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:dennis_ritchie][:languages] << 'Assembly'    
+programmer_hash
 
 end
