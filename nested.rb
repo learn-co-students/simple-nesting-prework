daed86cb88fdb610162fd00b786a160f53daf373
ko kristin <koberlander115@gmail.com>
2019-02-11 00:34:10 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -16,12 +17,11 @@ def hopper
         }
      }
 
-
+ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,10 +37,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+  programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -55,14 +56,13 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages] 
+  "C"
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
+ programmer_hash
 
 	programmer_hash = 
  		{
@@ -78,9 +78,13 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        
      }
-
-    
+programmer_hash = {:yukihiro_matsumoto => {}}
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",
+    :languages => []}
+programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"] 
+return programmer_hash
 end
 
 def changing_alan
