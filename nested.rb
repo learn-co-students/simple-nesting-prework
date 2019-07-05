fb396d429a76ce9f6d5fe39c503c4d1752b4c3b0
Michael Evans <mmevansjr@gmail.com>
2019-07-05 16:06:40 -0700

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,8 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
+#returns the value of grace hopper
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+programmer_hash[:alan_kay][:known_for]
+#returns the value "COLBOL".
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
 
 def adding_matz
@@ -79,7 +83,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+        
+    programmer_hash = {
+        :yukihiro_matsumoto => {
+            :known_for => "Ruby",
+            :languages => ["LISP, C"]
+            }
+        }
     
 end
 
@@ -101,8 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+        programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+        #Adding the word 'Assembly' to the next available index.
+        programmer_hash
 end
